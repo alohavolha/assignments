5a42ea87222fdbe861b2b26d4f9f0c81f97948fe
Volha Douban <douban@hawaii.edu>
2015-11-12 10:53:57 -1000

    small kine

--- week-12/exercise/script.js
+++ week-12/exercise/script.js
@@ -1,15 +1,18 @@
-var myName
-var student_1
-var student_2
-var student_3
-var student_4
-var student_5
-var student_6
-var student_7
-var student_8
-var student_9
-var student_10 
+var myName = 'Volha';
+var student_1 = 'Sam';
+var student_2 = 'Sasha';
+var student_3 = 'Hyela';
+var student_4 = 'Volha';
+var student_5 = 'Myung';
+var student_6 = 'Mayu';
+var student_7 = 'Yosh';
+var student_8 = 'Mark';
+var student_9 = 'Kalena';
+var student_10 = 'Jay';
 
-var intro = "Hello world, my name is"
+var intro = "Hello world, my name is ";
+var studentStatus = " is a current student";
 
-var studentStatus = "is a current student"
+for (var i = 0; i < 20; i++) {
+  console.log('yo');
+}
